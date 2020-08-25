4c04c702886be42a5b318687d891b360bf938335
Chethan S V <chethan.12371@gmail.com>
2020-08-25 20:31:31 +0530

    Update Col.cs

--- TelCo.ColorCoder/Col.cs
+++ TelCo.ColorCoder/Col.cs
@@ -7,43 +7,43 @@ namespace TelCo.ColorCoder
     {
         public static CPair GetColorFromPairNumber(int pairNumber)
         {
-            if (pairNumber < 1 || pairNumber > ColorCodeConstant.colorMapMinor.Length * ColorCodeConstant.colorMapMajor.Length)
+            if (pairNumber < 1 || pairNumber > CConstant.colorMapMinor.Length * CConstant.colorMapMajor.Length)
                 throw new ArgumentOutOfRangeException(string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-            int majorIndex = (pairNumber - 1) / ColorCodeConstant.colorMapMinor.Length;
-            int minorIndex = (pairNumber - 1) % ColorCodeConstant.colorMapMinor.Length;
+            int majorIndex = (pairNumber - 1) / CConstant.colorMapMinor.Length;
+            int minorIndex = (pairNumber - 1) % CConstant.colorMapMinor.Length;
 
             CPair pair = new CPair() 
             {
-                majorColor = ColorCodeConstant.colorMapMajor[majorIndex],
-                minorColor = ColorCodeConstant.colorMapMinor[minorIndex]
+                majorColor = CConstant.colorMapMajor[majorIndex],
+                minorColor = CConstant.colorMapMinor[minorIndex]
             };
             return pair;
         }
         public static int GetPairNumberFromColor(CPair pair)
         {
             int majorIndex = -1, minorIndex = -1;
-            for (int i = 0; i < ColorCodeConstant.colorMapMajor.Length; i++)
-                if (ColorCodeConstant.colorMapMajor[i] == pair.majorColor)
+            for (int i = 0; i < CConstant.colorMapMajor.Length; i++)
+                if (CConstant.colorMapMajor[i] == pair.majorColor)
                 {
                     majorIndex = i;
                     break;
                 }
-            for (int i = 0; i < ColorCodeConstant.colorMapMinor.Length; i++)
-                if (ColorCodeConstant.colorMapMinor[i] == pair.minorColor)
+            for (int i = 0; i < CConstant.colorMapMinor.Length; i++)
+                if (CConstant.colorMapMinor[i] == pair.minorColor)
                 {
                     minorIndex = i;
                     break;
                 }
             if (majorIndex == -1 || minorIndex == -1)
                 throw new ArgumentException(string.Format("Unknown Colors: {0}", pair.ToString()));
-            return (majorIndex * ColorCodeConstant.colorMapMinor.Length) + (minorIndex + 1);
+            return (majorIndex * CConstant.colorMapMinor.Length) + (minorIndex + 1);
         }
         public override string ToString() 
         {
             string colorCode = "";
             int i = 1;
-            foreach(var majorColor in ColorCodeConstant.colorMapMajor)
-                foreach(var minorColor in ColorCodeConstant.colorMapMinor)
+            foreach(var majorColor in CConstant.colorMapMajor)
+                foreach(var minorColor in CConstant.colorMapMinor)
                     colorCode +=$"MajorColor:{majorColor}, MinorColor:{minorColor}, PairNumber:{i++}\n";
             return colorCode;
         }
